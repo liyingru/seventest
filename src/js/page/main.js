41a3b5be7f96bef2e8aacf2a4636d8dc083fc00a
liyingru <2862031750@qq.com>
2018-11-07 08:30:21 +0800

    require完成

---
+++ src/js/page/main.js
@@ -0,0 +1,10 @@
+require.config({
+    baseUrl: './js/',
+    paths: {
+        "jquery": "jquery.min",
+        "require1": "require1",
+    }
+})
+
+
+require(['require1'])
---
+++ src/js/require1.js
@@ -0,0 +1,7 @@
+function require() {
+    //该方法可导入模块
+}
+
+function define() {
+    //该方法可以定义模块
+}
